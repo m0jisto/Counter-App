7ed695a2b3c5d5cbd1de9c22da45b49e0a945af9
Nikita <cheremuhinikita@mail.ru>
2020-10-01 12:58:18 +0300

    Updates

---
+++ precache-manifest.135aaa82a0887170a00d58392dc78b4c.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bce33cce90f673f6ffa6e2f46d9696",
+    "url": "/Counter-App/index.html"
+  },
+  {
+    "revision": "0696c195de80471a220f",
+    "url": "/Counter-App/static/js/main.64fff032.chunk.js"
+  },
+  {
+    "revision": "25cf27e640a424606a42",
+    "url": "/Counter-App/static/js/runtime-main.10d45f6f.js"
+  }
+]);
